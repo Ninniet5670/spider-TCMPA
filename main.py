f94aee2ac49770ea261c0e15bbe1bead2e38613a
Ninniet5670 <marconeto3000@gmail.com>
2024-04-28 12:42:58 -0300

    spider enhanced

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 
 driver = webdriver.Chrome()
@@ -13,12 +14,19 @@ driver.get('https://www.tcm.pa.gov.br/mural-de-licitacoes/')
 
 for i in range(1, 11):  # ajeitar para automação de geração automatica de arquivos (itera nas primeiras 10 paginas)
     a_tag_elements = driver.find_elements(By.XPATH, "//tr/td/a")
+    a_tag_strong_elements = driver.find_element(By.XPATH, "//tr/td/a/strong")
     a_tag_element_links = [a.get_attribute('href') for a in a_tag_elements]
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    strong_tags = soup.select("tr td a strong")
+    a_tag_strong_elements = [str(i).replace('<strong>', '').replace('</strong>', '').strip().replace('/', '.') 
+                             for i in list(strong_tags)]
+
 
     for j, a_tag_element in enumerate(a_tag_element_links):
         response_html = requests.get(a_tag_element).text
 
-        with open(f'bidding_page_{i}_record_{j+1}.html', 'w', encoding='utf-8') as bidding_page_record:
+        with open(f'{a_tag_strong_elements[j]}.html', 'w+', encoding='utf-8') as bidding_page_record:
             bidding_page_record.write(str(response_html))
         print("Arquivo HTML criado com sucesso!")
         time.sleep(.5)
